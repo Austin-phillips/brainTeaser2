4064cc75eaed3c60d35f7bbd8e46cd235b6e30cf
Austin Phillips <phillips.austin51@gmail.com>
2017-11-17 10:15:50 -0700

    almost working

--- brain_teaser2.rb
+++ brain_teaser2.rb
@@ -1,17 +1,45 @@
 require 'pry'
 
-def checker
+
+def menu
   puts 'Welcome to ruby Palindrome checker'
   puts 'Please insert word to check if it is a Palindrome'
+  checker
+end
+
+def checker
   choice = gets.strip.to_s
-  # Add regex to compare common letters in user input
-  # Output whether or not its a Palindrome
-  # loop the checker
-  if choice.match()
-    puts 'Palindrome'
+  if choice.match(/[A-Z]\w+/)
+    puts 'Not Palindrome, Try another word'
+    checker
   else
-    puts 'not palindrome, try another word'
+    puts 'Palindrome'
+    puts "to exit type 1 to try another word type 2"
+    option
   end
 end
 
-checker
+def option
+  option = gets.strip.to_i
+   if option == 1
+    exit
+   elsif option == 2
+    puts 'Please insert word to check if it is a Palindrome'
+    checker
+   end
+end
+
+menu
+
+# def checker
+#   choice = gets.strip.to_s
+#   if choice.match(/[A-Z]\w+/)
+#     puts 'Not Palindrome, Try another word'
+#     checker
+#   else
+#     puts 'Palindrome'
+#     puts "to exit type 1 to try another word type 2"
+#     option
+#   end
+# end
+
