a20d8c9b4f4dac36acf4f80db6e7b7e3f2905f89
Austin Phillips <phillips.austin51@gmail.com>
2017-11-17 09:55:09 -0700

    Sudo Code

--- brain_teaser2.rb
+++ brain_teaser2.rb
@@ -4,7 +4,10 @@ def checker
   puts 'Welcome to ruby Palindrome checker'
   puts 'Please insert word to check if it is a Palindrome'
   choice = gets.strip.to_s
-  if choice = choice
+  # Add regex to compare common letters in user input
+  # Output whether or not its a Palindrome
+  # loop the checker
+  if choice.match()
     puts 'Palindrome'
   else
     puts 'not palindrome, try another word'
